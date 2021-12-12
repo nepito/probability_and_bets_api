69cdaec918e2c5671e247df989805553b8cde11a
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-12 14:31:28 -0800

    🧪🛑 Add failing test

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -9,3 +10,9 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World from GECI!"}
+
+def test_pandas_to_dict():
+    a = {"a": [1,2]}
+    b = pd.DataFrame.from_dict(a)
+    c = pandas_to_dict(b)
+    pass
