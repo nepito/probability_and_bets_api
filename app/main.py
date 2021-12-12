a3682e7929bca7356602f96bc89e0171fe273ef5
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-12 14:26:09 -0800

    ✅ Test nothing

---
+++ app/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import json
+from fastapi import FastAPI, Body
+
+app = FastAPI()  # pragma: no mutate
+
+league_season_round = "78_2021_15"
+path_1 = f"data/bets_{league_season_round}.csv"
+path_2 = f"data/predictions_{league_season_round}.csv"
+bets = pd.read_csv(path_1)
+predicionts = pd.read_csv(path_2)
+matches = bets.join(predicionts.set_index("id_match"), on="id_match")
+to_render = [matches.iloc[i, :].to_dict() for i in range(len(matches))]
+
+@app.get("/")
+def read_root():
+    return {"msg": "Hello World from GECI!"}
