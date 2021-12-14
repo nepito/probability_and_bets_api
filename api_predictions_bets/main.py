3a2ed0b7d01b4c103960411ffd662fe6c6e9e897
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-13 20:26:47 -0800

    👩🏿‍💻 Parametrize bets and probabilities

--- api_predictions_bets/main.py
+++ api_predictions_bets/main.py
@@ -5,14 +5,16 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()  # pragma: no mutate
 
-league_season_round = "78_2021_15"
-path_1 = f"data/bets_{league_season_round}.csv"
-path_2 = f"data/predictions_{league_season_round}.csv"
-bets = pd.read_csv(path_1)
-conn = requests.get("https://uxxery.deta.dev/")
-res = conn.json()
-predicionts = pd.DataFrame.from_dict(res)
-matches = bets.join(predicionts.set_index("id_match"), on="id_match")
+league_season_round = "78_2021_16"
+def get_matches(league_season_round):
+    conn = requests.get(f"https://q98w4w.deta.dev/{league_season_round}")
+    res = conn.json()
+    bets = pd.DataFrame.from_dict(res)
+    conn = requests.get(f"https://uxxery.deta.dev/{league_season_round}")
+    res = conn.json()
+    predicionts = pd.DataFrame.from_dict(res)
+    matches = bets.join(predicionts.set_index("id_match"), on="id_match")
+    return matches
 
 
 def pandas_to_dict(a):
@@ -23,7 +25,8 @@ def pandas_to_list_of_dict(a):
     return [a.iloc[i, :].to_dict() for i in range(len(a))]
 
 
-@app.get("/")
-def read_root():
+@app.get("/{league_season_round}")
+def read_root(league_season_round):
+    matches = get_matches(league_season_round)
     to_render = pandas_to_list_of_dict(matches)
     return {"response": to_render}
